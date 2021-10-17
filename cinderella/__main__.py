8f1087b84bc796bcc294d3ab381aba4865c2172d
its parv <90262471+coderparv@users.noreply.github.com>
2021-10-17 20:32:55 +0530

    Update __main__.py

--- cinderella/__main__.py
+++ cinderella/__main__.py
@@ -26,12 +26,23 @@ from cinderella.modules.connection import connected
 from cinderella.modules.connection import connect_button
 
 
-PM_START_TEXT = """
-_Hello_ *{}*
-_My name is_ *{}*\n_A Powerful Telegram ProBot to Manage Your Groups,feel free to add to your groups!!_
-_Maintained by_ [{}](tg://user?id={})
+PM_START_TEXT = f"
+` ding dong Well to know you are here to meet me.
+Myself Central bot made for @CENTRAL_COMMUNITY to manage it.
+i am for private use not for every public group go to the buttons below ..'
 """
 
+buttons = [
+    [
+        InlineKeyboardButton(
+            text="🈴CENTRAL COMMUNITY", url="t.me/python_pirates_bot?startgroup=true"),
+    ],
+    [
+        InlineKeyboardButton(text="MY CREATOR 🔧", url="https://t.me/parv200v"),
+        InlineKeyboardButton(text="Source code🗂", url="https://github.com/coderparv/central"),◽️
+    ],
+     
+]
 
 HELP_STRINGS = """
 Hey there! My name is *{}*.
